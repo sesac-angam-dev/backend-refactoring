6394c5b1fa9a8b679e71d0bdfbbe49dc0c2ab218
JINU-CHANG <jenny010925@naver.com>
2024-01-06 18:56:45 +0900

    first commit

---
+++ src/test/java/com/sesac/angam/AngamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesac.angam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
